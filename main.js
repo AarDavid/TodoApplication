690198f8129864c2dc3167721a118731834cbac2
David Aarlien <davidaarlienxnor@hotmail.no>
2020-09-02 14:23:57 +0200

    Finished Js functionalities for todolist

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 var modal = document.getElementById("modal");
+var todoListDiv = document.getElementById("todoList");
+var title = "";
+var description = "";
+var author = "";
+
+var todoList = [];
+var completedTodoList = [];
 
 document.getElementById("createTodoBtn").addEventListener("click", function() {
     modal.style.display = "block";
@@ -8,8 +15,98 @@ document.addEventListener("click", function() {
     if(event.target == modal) {
         modal.style.display = "none";
     }
-})
+});
 
 document.getElementById("closeCreateTodo").addEventListener("click", function() {
     modal.style.display = "none";
-})
+});
+
+function getTodoInputData() {
+    title = document.getElementById("todoTitle").value;
+    description = document.getElementById("todoDescription").value;
+    author = document.getElementById("todoAuthor").value;
+}
+
+document.getElementById("createTodoConfirmBtn").addEventListener("click", function() {
+    getTodoInputData();
+
+    todoList.push ({
+        "title": title,
+        "description": description,
+        "author": author
+    });
+
+    printDataTodos();
+
+    console.log(todoList);
+
+    modal.style.display = "none";
+});
+
+function printDataTodos() {
+    todoListDiv.innerHTML = "";
+    for(var i = 0; i < todoList.length; i++){
+        var output = "<div class='todoElement' value='" + i + "'>" + "<h1>" + todoList[i].title + "</h1>" + "<p>" + todoList[i].description + "</p>"
+        + "<div class='todoElementBtns'><button onclick='removeElement(this)' class='removeTodoBtn'>Delete</button><button onclick='completeElement(this)' class='completeTodoBtn'>Complete</button></div>"
+        + "</div>";
+        
+        todoListDiv.innerHTML += output;
+    }
+}
+
+
+function removeElement(element) {
+    var currentElement =  element.parentNode.parentNode;
+   
+    for(var i = 0; i < todoList.length; i++) {
+        if(parseInt(currentElement.getAttribute("value")) === i) {
+            todoList.splice(i, 1);
+        }
+    }
+    currentElement.remove();
+    printDataTodos();
+
+    console.log(todoList);
+}
+
+
+
+function printDataCompletedTodos() {
+    var completedTodoListDiv = document.getElementById("listElements");
+    completedTodoListDiv.innerHTML = "";
+
+    for(var i = 0; i < completedTodoList.length; i++){
+        var output = "<div class='listRow listElement'>" + "<p>" + completedTodoList[i].title + "</p>" + "<p>" + completedTodoList[i].description + "</p>"
+        + "<p>" + completedTodoList[i].author + "</p>" + "<p> Not yet</p>"
+        
+        completedTodoListDiv.innerHTML += output;
+
+    }
+}
+
+
+function completeElement(element) {
+    var currentElement = element.parentNode.parentNode;
+    var currentElementValue = currentElement.getAttribute("value");
+    
+
+    for(var i = 0; i < todoList.length; i++) {
+        if(parseInt(currentElementValue) === i) {
+            completedTodoList.push ({
+                "title": todoList[i].title,
+                "description": todoList[i].description,
+                "author": todoList[i].author
+            });
+
+            todoList.splice(i, 1);
+            printDataTodos();
+            printDataCompletedTodos();
+            currentElement.remove();
+        }
+    }
+
+    console.log("Todo list")
+    console.log(todoList);
+    console.log("Completed todo list")
+    console.log(completedTodoList);
+}
